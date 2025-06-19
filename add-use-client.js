1a809e033a94acc5f3e668a25a49aa7ca652a8ae
arzhannikov <artcrazy.mail@gmail.com>
2025-06-20 00:02:05 +0300

    ref

--- add-use-client.js
+++ add-use-client.js
@@ -1,17 +1,17 @@
-// const fs = require('fs');
-// const path = require('path');
-//
-// const files = [
-//   path.join(__dirname, 'dist', 'Header.js'),
-//   // Если потребуется, добавьте сюда другие клиентские компоненты
-// ];
-//
-// files.forEach((file) => {
-//   if (fs.existsSync(file)) {
-//     const content = fs.readFileSync(file, 'utf8');
-//     if (!content.startsWith("'use client';")) {
-//       fs.writeFileSync(file, `'use client';\n${content}`);
-//       console.log(`'use client' добавлен в ${file}`);
-//     }
-//   }
-// });
+const fs = require('fs');
+const path = require('path');
+
+const files = [
+  path.join(__dirname, 'dist', 'Header.js'),
+  // Если потребуется, добавьте сюда другие клиентские компоненты
+];
+
+files.forEach((file) => {
+  if (fs.existsSync(file)) {
+    const content = fs.readFileSync(file, 'utf8');
+    if (!content.startsWith("'use client';")) {
+      fs.writeFileSync(file, `'use client';\n${content}`);
+      console.log(`'use client' добавлен в ${file}`);
+    }
+  }
+});
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 import styles from './Header.module.scss';
 import Search from './Search/Search';
 import MobileNav from './MobileNav/MobileNav';
 import NavItem from "../Navigation/NavItem";
 
 const Header = () => {
+  const pathname = usePathname();
 
-  // const handleLogoClick = (e: React.MouseEvent) => {
-  //   // if (pathname === '/') {
-  //     e.preventDefault();
-  //     window.location.reload();
-  //   // }
-  // };
+  const handleLogoClick = (e: React.MouseEvent) => {
+    if (pathname === '/') {
+      e.preventDefault();
+      window.location.reload();
+    }
+  };
 
   return (
     <>
       <header className={styles.header}>
-        <Link href="/" className={styles.logo}
-              // onClick={handleLogoClick}
-        >
+        <Link href="/" className={styles.logo} onClick={handleLogoClick}>
           <span role="img" aria-label="Логотип">🍴</span> Что приготовить
         </Link>
         <nav className={styles.headerRight} aria-label="Основная навигация">
