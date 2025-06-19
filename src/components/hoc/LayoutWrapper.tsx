6313cdc56a36d2616507e8b172244cae4088151c
Zloy <108714979+MalenkoZloy@users.noreply.github.com>
2025-06-19 15:48:41 +0700

    поправил scss

--- src/components/hoc/LayoutWrapper.tsx
+++ src/components/hoc/LayoutWrapper.tsx
@@ -1,7 +1,7 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import styles from './LayoutWrapper.module.scss';
-import { ReactNode } from 'react';
+import {ReactNode} from 'react';
 
 interface LayoutProps {
     children: ReactNode;
