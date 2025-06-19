ff3ddd748c9d0f45c451317319b4eff8a0da000a
arzhannikov <artcrazy.mail@gmail.com>
2025-06-20 01:43:31 +0300

    ref

--- src/components/hoc/LayoutWrapper.tsx
+++ src/components/hoc/LayoutWrapper.tsx
@@ -1,5 +1,5 @@
 // import Header from '../Header/Header';
-// import Footer from '../Footer/Footer';
+import Footer from '../Footer/Footer';
 import styles from './LayoutWrapper.module.scss';
 import {ReactNode} from 'react';
 
@@ -12,6 +12,6 @@ export const LayoutWrapper  = ({ children }: LayoutProps) => (
         <div>test</div>
         {/*<Header/>*/}
         <main className={styles.main}>{children}</main>
-        {/*<Footer/>*/}
+        <Footer/>
     </>
 );
