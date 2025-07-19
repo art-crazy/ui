9bd142482a5b8703b25ec2f91edd31cb7aa387fb
arzhannikov <artcrazy.mail@gmail.com>
2025-07-20 01:44:08 +0300

    Add home's components

--- src/components/ContainerHome/ContainerHome.tsx
+++ src/components/ContainerHome/ContainerHome.tsx
@@ -1,7 +1,8 @@
 import styles from './ContainerHome.module.scss';
+import {ReactNode} from "react";
 
-export const ContainerHome = () => {
+export const ContainerHome = ({children}: {children: ReactNode}) => {
     return (
-        <div className={styles.home}/>
+        <div className={styles.home}>{children}</div>
     )
 }
