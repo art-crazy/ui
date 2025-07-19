62494da3ae0bf818bb664493094b3ab25a0e82d9
arzhannikov <artcrazy.mail@gmail.com>
2025-07-19 13:06:26 +0300

    Fix type Breadcrumbs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-export { Breadcrumbs } from "@/components/Breadcrumbs";
-export { RecipeListSection } from "@/components/Filters/components/RecipeList/RecipeListSection";
-export { ButtonSearch } from "@/components/Filters/components/ButtonSearch";
-export { FilterGroup } from "@/components/Filters/components/FilterGroup";
+// Алиасы здесь не настроены
+
+export { Breadcrumbs } from "./components/Breadcrumbs";
+export { RecipeListSection } from "./components/Filters/components/RecipeList/RecipeListSection";
+export { ButtonSearch } from "./components/Filters/components/ButtonSearch";
+export { FilterGroup } from "./components/Filters/components/FilterGroup";
 export { LayoutWrapperUI } from './components/hoc/LayoutWrapperUI';
 export { HeaderUI } from './components/Header/HeaderUI';
 export { Mix } from './page/mix/Mix'
 export { Container } from './components/Container'
 export { ReceptyContent } from './components/ReceptyContent'
 export { Filters } from './components/Filters'
-export { RecipeListAll } from "@/components/Filters/components/RecipeList/RecipeListSection/RecipeListAll";
-export { RecipeList } from "@/components/Filters/components/RecipeList";
+export { RecipeListAll } from "./components/Filters/components/RecipeList/RecipeListSection/RecipeListAll";
+export { RecipeList } from "./components/Filters/components/RecipeList";
